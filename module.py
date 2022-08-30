f0675857f71aa01a732e036ffa1b8ca2d16dcb58
Lino-ver2 <86415531+Lino-ver2@users.noreply.github.com>
2022-08-31 04:19:24 +0900

    conv1d under development

--- module.py
+++ module.py
@@ -2,41 +2,43 @@ import numpy as np
 np.random.seed(42)
 
 
+import numpy as np
+np.random.seed(42)
+
+
 class Affine():
     def __init__(self, in_dim: int, out_dim: int):
         """
         in_dim : explanatory_variable_dim_
         out_dim : target_variable_dim_
-        w : weights
-        b : bias
-        dw : parameter_gradient
-        db : bias_gradient
+        dw : parameter_gradient_
+        db : bias_gradient_
         """
         self.in_dim = in_dim
         self.out_dim = out_dim
         
-        self.w = np.random.randn(self.in_dim, self.out_dim)
-        self.b = np.zeros(self.out_dim, dtype=float)
+        self.weight = np.random.randn(in_dim, out_dim)
+        self.bias = np.zeros(out_dim, dtype=float)
 
         self.dx , self.dw, self.db = None, None, None
 
     def __call__(self, x: np.ndarray) -> np.ndarray:
         """ forward_propagation
         x : input_data_ (batch_size, in_dim)
-        out : target_data_ (batch_size, out_dim)
+        output : output_data_ (batch_size, out_dim)
         """
         self.x = x
-        self.out = np.dot(self.x, self.w) + self.b
-        self.param = {'w' : self.w, 'b' : self.b}
-        return  self.out
+        output = np.dot(self.x, self.weight) + self.bias
+        self.param = {'w' : self.weight, 'b' : self.bias}
+        return  output
 
     def backward(self, grad: np.ndarray) -> np.ndarray:
         """ back_propagation
         grad : previous_gradient_ (batch_size, out_dim)
         dx : gradient_ (batch_size, in_dim)
         """
-        self.dx = np.dot(grad, self.w.T)
-        self.dw = np.dot(self.x.T, grad)
-        self.db = np.sum(self.b)
-        self.grad_param = {'x' : self.dx, 'w' : self.dw, 'b' : self.db}
-        return self.dx
+        dx = np.dot(grad, self.weight.T)
+        dw = np.dot(self.x.T, grad)
+        db = np.sum(self.bias)
+        self.grad_param = {'w' : dw, 'b' : db}
+        return dx
